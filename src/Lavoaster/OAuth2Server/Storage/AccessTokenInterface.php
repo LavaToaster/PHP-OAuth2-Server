54375d69b7b215516f73875251291d34b7fde93e
Adam Lavin <adam@lavoaster.co.uk>
2013-09-23 17:39:53 +0100

    All Token interfaces now extend TokenInterface

--- src/Lavoaster/OAuth2Server/Storage/AccessTokenInterface.php
+++ src/Lavoaster/OAuth2Server/Storage/AccessTokenInterface.php
@@ -1,6 +1,6 @@
 <?php namespace Lavoaster\OAuth2Server\Storage;
 
-interface AccessTokenInterface
+interface AccessTokenInterface extends TokenInterface
 {
 	/**
 	 * Returns the access token
@@ -16,79 +16,4 @@ interface AccessTokenInterface
 	 * @return void
 	 */
 	public function setAccessToken($accessToken);
-
-	/**
-	 * Returns the OAuth Client
-	 *
-	 * @return ClientInterface
-	 */
-	public function getClient();
-
-	/**
-	 * Sets the client that owns the token
-	 *
-	 * @param ClientInterface $client
-	 * @return void
-	 */
-	public function setClient(ClientInterface $client);
-
-	/**
-	 * Returns the user bound to the token
-	 *
-	 * @return OAuthUserInterface
-	 */
-	public function getUser();
-
-	/**
-	 * Sets the user assigned to the token
-	 *
-	 * @param OAuthUserInterface $user
-	 * @return void
-	 */
-	public function setUser(OAuthUserInterface $user);
-
-	/**
-	 * Returns the token expiry date
-	 *
-	 * @return string
-	 */
-	public function getExpiry();
-
-	/**
-	 * Sets the tokens expiry date
-	 *
-	 * @param string $expiry
-	 * @return void
-	 */
-	public function setExpiry($expiry);
-
-	/**
-	 * Checks if the access token is expired
-	 *
-	 * @return bool
-	 */
-	public function isExpired();
-
-	/**
-	 * Returns the scopes the token has access to
-	 *
-	 * @return array
-	 */
-	public function getScopes();
-
-	/**
-	 * Sets the scopes on the access token
-	 *
-	 * @param array $scopes
-	 * @return void
-	 */
-	public function setScopes(array $scopes);
-
-	/**
-	 * Checks if the token has access to a given scope
-	 *
-	 * @param string $scope
-	 * @return bool
-	 */
-	public function hasScope($scope);
 }
--- src/Lavoaster/OAuth2Server/Storage/AuthorizationCodeInterface.php
+++ src/Lavoaster/OAuth2Server/Storage/AuthorizationCodeInterface.php
@@ -1,6 +1,6 @@
 <?php namespace Lavoaster\OAuth2Server\Storage;
 
-interface AuthorizationCodeInterface
+interface AuthorizationCodeInterface extends TokenInterface
 {
 	/**
 	 * Returns the authorization code
@@ -18,36 +18,6 @@ interface AuthorizationCodeInterface
 	public function setAuthorizationCode($code);
 
 	/**
-	 * Returns the OAuth Client
-	 *
-	 * @return ClientInterface
-	 */
-	public function getClient();
-
-	/**
-	 * Sets the client that owns the token
-	 *
-	 * @param ClientInterface $client
-	 * @return void
-	 */
-	public function setClient(ClientInterface $client);
-
-	/**
-	 * Returns the user bound to the token
-	 *
-	 * @return OAuthUserInterface
-	 */
-	public function getUser();
-
-	/**
-	 * Sets the user assigned to the token
-	 *
-	 * @param OAuthUserInterface $user
-	 * @return void
-	 */
-	public function setUser(OAuthUserInterface $user);
-
-	/**
 	 * Returns the redirect uri
 	 *
 	 * @return mixed
@@ -61,49 +31,4 @@ interface AuthorizationCodeInterface
 	 * @return void
 	 */
 	public function setRedirectUri($redirectUri);
-
-	/**
-	 * Returns the token expiry date
-	 *
-	 * @return string
-	 */
-	public function getExpiry();
-
-	/**
-	 * Sets the tokens expiry date
-	 *
-	 * @param string $expiry
-	 * @return void
-	 */
-	public function setExpiry($expiry);
-
-	/**
-	 * Checks if the access token is expired
-	 *
-	 * @return boolean
-	 */
-	public function isExpired();
-
-	/**
-	 * Returns the scopes the token has access to
-	 *
-	 * @return array
-	 */
-	public function getScopes();
-
-	/**
-	 * Sets the scopes on the access token
-	 *
-	 * @param array $scopes
-	 * @return void
-	 */
-	public function setScopes(array $scopes);
-
-	/**
-	 * Checks if the token has access to a given scope
-	 *
-	 * @param string $scope
-	 * @return bool
-	 */
-	public function hasScope($scope);
 }
