18fd3d94993206a8c520855a133b602cb61801a7
Adam Lavin <adam@lavoaster.co.uk>
2013-09-23 19:26:26 +0100

    Added user.class to config and added descriptions

--- src/config/config.php
+++ src/config/config.php
@@ -1,9 +1,12 @@
 <?php
 
+/* TODO: Expand on descriptions and format them to laravel style */
+
 return [
 	'user' => [
-		'id' => 'id',
-		'table' => 'users',
-		'identifier' => 'username'
+		'id' => 'id', // Column that stores the user id
+		'table' => 'users', // Table that contains the users
+		'identifier' => 'username', // Column to use when identifying users through the credentials grant type
+		'class' => 'User' // Class that implements OAuthUserInterface so the library can query your user table
 	]
 ];
