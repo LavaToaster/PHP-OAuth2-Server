619bbd3e6cfc3d772bffee038983e973e17c4216
Adam Lavin <adam@lavoaster.co.uk>
2013-09-23 16:34:35 +0100

    Starting over, removing everything not needed

--- src/Lavoaster/OAuth2Server/User/LaravelUserPasswordValidator.php
+++
@@ -1,9 +0,0 @@
-<?php namespace Lavoaster\OAuth2Server\User;
-
-class LaravelUserPasswordValidator implements UserPasswordValidatorInterface
-{
-	public function checkPassword(array $user, $password)
-	{
-		return \Hash::check($password, $user['password']);
-	}
-}
--- src/Lavoaster/OAuth2Server/User/UserPasswordValidatorInterface.php
+++
@@ -1,6 +0,0 @@
-<?php namespace Lavoaster\OAuth2Server\User;
-
-interface UserPasswordValidatorInterface
-{
-	public function checkPassword(array $user, $password);
-}
