acb94e41291cdc2a01785eeb576f25665bf1079d
Adam Lavin <adam@lavoaster.co.uk>
2013-09-24 02:50:23 +0100

    Added config for User repo / storage

--- src/config/config.php
+++ src/config/config.php
@@ -27,6 +27,7 @@ return [
 		'id' => 'id', // Column that stores the user id
 		'table' => 'users', // Table that contains the users
 		'identifier' => 'username', // Column to use when identifying users through the credentials grant type
-		'class' => 'User' // Class that implements OAuthUserInterface so the library can query your user table
+		'storage' => 'User', // Class that implements OAuthUserInterface so the library can query your user table
+		'repository' => 'User'
 	]
 ];
