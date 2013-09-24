bf77de73d5ca9472672d5d5c2d42cd74c48f6383
Adam Lavin <adam@lavoaster.co.uk>
2013-09-24 02:53:37 +0100

    Setup the constructor for the OAuthServer class

--- src/Lavoaster/OAuth2Server/OAuth2Server.php
+++ src/Lavoaster/OAuth2Server/OAuth2Server.php
@@ -1,11 +1,51 @@
 <?php namespace Lavoaster\OAuth2Server;
 
+use Lavoaster\OAuth2Server\Repositories\AccessTokenRepositoryInterface;
+use Lavoaster\OAuth2Server\Repositories\ClientRepositoryInterface;
+use Lavoaster\OAuth2Server\Repositories\Eloquent\AuthorizationCodeRepository;
+use Lavoaster\OAuth2Server\Repositories\OAuthUserRepositoryInterface;
+use Lavoaster\OAuth2Server\Repositories\RefreshTokenRepositoryInterface;
 
 class OAuth2Server {
 
-	public function __construct()
+	/**
+	 * @var Repositories\OAuthUserRepositoryInterface
+	 */
+	protected $userRepository;
+
+	/**
+	 * @var Repositories\AccessTokenRepositoryInterface
+	 */
+	protected $accessTokenRepository;
+
+	/**
+	 * @var Repositories\RefreshTokenRepositoryInterface
+	 */
+	protected $refreshTokenRepository;
+
+	/**
+	 * @var Repositories\ClientRepositoryInterface
+	 */
+	protected $clientRepository;
+
+	/**
+	 * @var Repositories\Eloquent\AuthorizationCodeRepository
+	 */
+	protected $authorizationCodeRepository;
+
+	public function __construct(
+		OAuthUserRepositoryInterface $userRepository,
+		AccessTokenRepositoryInterface $accessTokenRepository,
+		RefreshTokenRepositoryInterface $refreshTokenRepository,
+		ClientRepositoryInterface $clientRepository,
+		AuthorizationCodeRepository $authorizationCodeRepository
+	)
 	{
-
+		$this->userRepository = $userRepository;
+		$this->accessTokenRepository = $accessTokenRepository;
+		$this->authorizationCodeRepository = $authorizationCodeRepository;
+		$this->refreshTokenRepository = $refreshTokenRepository;
+		$this->clientRepository = $clientRepository;
+		$this->authorizationCodeRepository = $authorizationCodeRepository;
 	}
-
 }
