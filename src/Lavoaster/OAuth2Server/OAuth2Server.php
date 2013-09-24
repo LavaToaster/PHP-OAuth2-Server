4cb62c0906b0364bbfe64c4633c1bbb6caba87ad
Adam Lavin <adam@lavoaster.co.uk>
2013-09-24 02:43:08 +0100

    Forgot to remove the imports for these.

--- src/Lavoaster/OAuth2Server/OAuth2Server.php
+++ src/Lavoaster/OAuth2Server/OAuth2Server.php
@@ -1,17 +1,5 @@
 <?php namespace Lavoaster\OAuth2Server;
 
-use Config;
-use Illuminate\Http\JsonResponse;
-use Illuminate\Http\RedirectResponse;
-use Response as LaravelResponse;
-use Lavoaster\OAuth2Server\HttpBridge\Request;
-use Lavoaster\OAuth2Server\HttpBridge\Response;
-use Lavoaster\OAuth2Server\Storage\Laravel;
-use OAuth2\GrantType\AuthorizationCode;
-use OAuth2\GrantType\ClientCredentials;
-use OAuth2\GrantType\JwtBearer;
-use OAuth2\GrantType\UserCredentials;
-use OAuth2\Server;
 
 class OAuth2Server {
 
