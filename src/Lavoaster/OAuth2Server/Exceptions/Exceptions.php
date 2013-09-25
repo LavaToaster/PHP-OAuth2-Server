aef4298c41c1a6170b966b4f4f0a4b05982b4b6f
Adam Lavin <adam@lavoaster.co.uk>
2013-09-25 03:32:56 +0100

    Added empty Exceptions file.

---
+++ src/Lavoaster/OAuth2Server/Exceptions/Exceptions.php
@@ -0,0 +1,5 @@
+<?php namespace LavoasterOAuth2Server\Exceptions;
+
+//COMING IN THE YEAR 2013, OAuth Exceptions: The Musical.
+//In all seriousness, they'll be here when I get around to
+//determining if exceptions will be useful :)
